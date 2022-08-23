6f4e9442bfc89e83e6d6e86d6c6ac0b54cf74127
aumetov7 <aumetov7@gmail.com>
2022-08-23 17:38:17 +0600

    Add search bar and fake data

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Акбар Уметов on 23/8/22.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
